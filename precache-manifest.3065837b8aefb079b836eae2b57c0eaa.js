f748454f1a11f8418670aa6c9dfc046290e7b0e2
M4x5ch31612 <max.schonert@student.hs-anhalt.de>
2022-09-30 10:47:52 +0200

    Updates

---
+++ precache-manifest.3065837b8aefb079b836eae2b57c0eaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddb63b056cf63e60e0f4",
+    "url": "/Abschlussprojekt/static/css/main.6c647830.chunk.css"
+  },
+  {
+    "revision": "ddb63b056cf63e60e0f4",
+    "url": "/Abschlussprojekt/static/js/main.ddb63b05.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/Abschlussprojekt/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "0ae8b261d27d39e24071",
+    "url": "/Abschlussprojekt/static/js/2.0ae8b261.chunk.js"
+  },
+  {
+    "revision": "a809bc8faac2f82dd73e",
+    "url": "/Abschlussprojekt/static/js/runtime~main.a809bc8f.js"
+  },
+  {
+    "revision": "5de1c0338ecd6f08e9971c1b991f72b1",
+    "url": "/Abschlussprojekt/static/media/amazon_black.5de1c033.png"
+  },
+  {
+    "revision": "183e64fd8482f12d2219136f3db669f4",
+    "url": "/Abschlussprojekt/static/media/amazon_white.183e64fd.png"
+  },
+  {
+    "revision": "e05270ff3068789e03e18c716538ef0e",
+    "url": "/Abschlussprojekt/static/media/404.e05270ff.jpg"
+  },
+  {
+    "revision": "223c45aea9ff4d8c2d677b3f55fe4567",
+    "url": "/Abschlussprojekt/index.html"
+  }
+];
